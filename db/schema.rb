85430bed688763aabccca4478b9d4c8b278f1182
Jack Deters <jdeters7@gmail.com>
2018-10-05 16:30:34 -0500

    devise user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_05_202658) do
+ActiveRecord::Schema.define(version: 2018_10_05_212011) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2018_10_05_202658) do
     t.string "nickname"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
